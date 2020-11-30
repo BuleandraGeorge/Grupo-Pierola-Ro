e18f5938206219953ad2f214888cd000bdd8bbe2
BuleandraGeorge <buleandrageorge@gmail.com>
2020-11-30 18:23:40 +0000

    Set secret key and debug

--- GPR/settings.py
+++ GPR/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '9%6%(uj4aw&sdlnx7zef&2_!+e_tq=4kgeul9o%o@vb@e2f^4v'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPEMENT' in os.environ
 
 ALLOWED_HOSTS = ['grupo-pierola-ro.herokuapp.com', 'localhost']
 

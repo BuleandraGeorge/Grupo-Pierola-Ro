073d1695c3b038f4cae927c92e66f3cd649f752e
BuleandraGeorge <buleandrageorge@gmail.com>
2020-12-01 19:41:46 +0000

    Edit typo USER_HOST TO EMAIL_HOST

--- GPR/settings.py
+++ GPR/settings.py
@@ -223,7 +223,7 @@ else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     USER_USE_TLS = True
     USER_PORT = 587
-    USER_HOST = 'smtp.gmail.com'
+    EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
